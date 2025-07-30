329a482e647b149cb95400d344f326fbe1215041
cesar9898 <graficacevallos@gmail.com>
2025-07-30 03:51:51 +0000

    creacion de de tablas en models

--- empleados/models.py
+++ empleados/models.py
@@ -1,7 +1,18 @@
 class tipousuario (models.Model):
 
   class gerente  (models.Model):
-      contrase
-      
-     
+     contrasena = models.CharField(max_length=100)
+     usuario = models.CharField(max_length=100, unique=True)
    
+     
+   class empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    fecha_contratacion = models.DateTimeField(auto_now_add=True)
+    tipo_usuario = models.ForeignKey(tipousuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+      
